527b605d06b1264ea797726daf697846a63ca069
lazija <60522890+lazija@users.noreply.github.com>
2021-04-19 12:43:06 +0200

    Add files via upload

---
+++ izlistaj.py
@@ -0,0 +1,8 @@
+import os
+path = 'E:/3D_BODY/3d_model/hips_and_legs1to10'
+files = os.listdir(path)
+x="}},"
+for f in files:
+    with open('dodaj_text.txt','a+', encoding="utf-8") as text_file:
+        text_file.write('{Filename: "%s", Class : "#b1f2ff", wikipediaInfos : { en: "Left_coronary_artery", sr: "Артерија"'%f+x+'\n')
+print("Gotovo, sve dodato u datoteku 'dodaj_text.txt'")
